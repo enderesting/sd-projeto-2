a81311adb7378c456e9a80f6838b8db74f7f9f30
Emily Sá <fc58200@alunos.fc.ul.pt>
2023-09-27 20:29:23 +0100

    Added data.c and defined two of the functions

---
+++ src/data.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <string.h>
+#include "data.h"
+
+/* Função que cria um novo elemento de dados data_t e que inicializa 
+ * os dados de acordo com os argumentos recebidos, sem necessidade de
+ * reservar memória para os dados.	
+ * Retorna a nova estrutura ou NULL em caso de erro.
+ */
+struct data_t *data_create(int size, void *data){
+    struct data_t* d;
+    
+    if (size <= 0 || data == NULL)
+        return NULL;
+
+    d = malloc (sizeof(struct data_t));
+
+    d->data = data;
+    d->datasize = size;
+
+    return d;
+}
+
+/* Função que elimina um bloco de dados, apontado pelo parâmetro data,
+ * libertando toda a memória por ele ocupada.
+ * Retorna 0 (OK) ou -1 em caso de erro.
+ */
+int data_destroy(struct data_t *data){
+
+    if(data == NULL)
+        return -1;
+
+    free(data);    
+
+    return 0;
+}
+
+/* Função que duplica uma estrutura data_t, reservando a memória
+ * necessária para a nova estrutura.
+ * Retorna a nova estrutura ou NULL em caso de erro.
+ */
+struct data_t *data_dup(struct data_t *data){
+
+    return NULL;
+}
+
+/* Função que substitui o conteúdo de um elemento de dados data_t.
+ * Deve assegurar que liberta o espaço ocupado pelo conteúdo antigo.
+ * Retorna 0 (OK) ou -1 em caso de erro.
+ */
+int data_replace(struct data_t *data, int new_size, void *new_data){
+
+    return 0;
+}
