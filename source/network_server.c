ef967dc3294b6f73da3d8eec40b76c575184d99d
enderesting <datendza@gmail.com>
2023-10-26 20:05:41 +0100

    added SIGPIPE handling + setsockopt(), added missing struct

--- source/network_server.c
+++ source/network_server.c
@@ -1,12 +1,52 @@
+//network server
 
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+//extra
 #include "network_server.h"
+#include "table_skel.h"
 #include "sdmessage.pb-c.h"
+
+//tamanho maximo da mensagem enviada pelo cliente
+#define MAX_MSG 2048
 
 /* Função para preparar um socket de receção de pedidos de ligação
  * num determinado porto.
  * Retorna o descritor do socket ou -1 em caso de erro.
  */
-int network_server_init(short port);
+int network_server_init(short port){
+    // also receives some listsize
+    int listsize;
+    // struct table_t* table = table_skel_init(listsize);
+    
+    network_main_loop(port, table_skel_init(listsize));
+    int sockfd, connsockfd;
+    if (sockfd=socket(AF_INET, SOCK_STREAM,0)<0){
+        perror("Erro ao criar socket");
+        return -1;
+    }
+    //https://linux.die.net/man/3/setsockopt <- reference
+    //https://stackoverflow.com/questions/24194961/how-do-i-use-setsockoptso-reuseaddr
+    //this setting is used to make sure local address can be reused
+    //SOL_SOCKET:   - set options at socket level (unsure?)
+    //SO_REUSEADDR  - address can be reused
+    //&(int){1}     - 1 as in enable the previous option
+    //sizeof(int)   - specification indicates that this option takes an int value
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int))==-1){
+        error("setsockopt(SO_REUSEADDR) failed");
+        return -1;
+    }
+
+    return sockfd;
+
+}
 
 /* A função network_main_loop() deve:
  * - Aceitar uma conexão de um cliente;
@@ -18,7 +58,26 @@ int network_server_init(short port);
  * A função não deve retornar, a menos que ocorra algum erro. Nesse
  * caso retorna -1.
  */
-int network_main_loop(int listening_socket, struct table_t *table);
+int network_main_loop(int listening_socket, struct table_t *table){
+    // handling SIGPIPE
+    // sigaction(SIGPIPE, &(sigaction){sigpipe_handler}, NULL); // CHECKTHIS: should we use sigaction instead?
+    // reference: https://stackoverflow.com/questions/18935446/program-received-signal-sigpipe-broken-pipe?noredirect=1&lq=1
+    signal(SIGPIPE,signal_handler); //CHECKTHIS: should this be in table_server instead?
+
+    //acccept a client
+    int ret = 0;
+    while(ret != -1){
+        //receive a message, deserialize it
+        MessageT *msg = network_receive(listening_socket);
+        //get table_skel to process and get response
+        invoke(msg,table);
+        //wait until response is here?
+        int res = network_send(listening_socket, msg);
+    }
+    //close shop baby
+    table_skel_destroy(table);
+    network_server_close(listening_socket); //does this go first?
+}
 
 /* A função network_receive() deve:
  * - Ler os bytes da rede, a partir do client_socket indicado;
@@ -26,17 +85,35 @@ int network_main_loop(int listening_socket, struct table_t *table);
  *   reservando a memória necessária para a estrutura MessageT.
  * Retorna a mensagem com o pedido ou NULL em caso de erro.
  */
-MessageT *network_receive(int client_socket);
+MessageT *network_receive(int client_socket){
+    // read short first
+    // and then the rest of the socket 
+
+}
 
 /* A função network_send() deve:
  * - Serializar a mensagem de resposta contida em msg;
  * - Enviar a mensagem serializada, através do client_socket.
  * Retorna 0 (OK) ou -1 em caso de erro.
  */
-int network_send(int client_socket, MessageT *msg);
+int network_send(int client_socket, MessageT *msg){
+
+}
 
 /* Liberta os recursos alocados por network_server_init(), nomeadamente
  * fechando o socket passado como argumento.
  * Retorna 0 (OK) ou -1 em caso de erro.
  */
-int network_server_close(int socket);
+int network_server_close(int socket){
+}
+
+
+void signal_handler(int signal) //TODO: add header of this 
+{
+    if (signal == SIGPIPE){
+        printf("Caught SIGPIPE, ignore and continue"); 
+        // since we want it to ignore, eventually we can swap this function out for SIG_IGN instead...
+        // however, for the sake of testing,there's a print here. 
+        // SIGPIPE: Broken pipe: write to pipe with no readers
+    }
+}
--- source/table_server.c
+++ source/table_server.c
@@ -4,12 +4,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+// #include <signal.h>
 #include "inet.h"
 #include "table_server.h"
 
 
 int main(int argc, char *argv[]) {
-    //handles args
+    //processing args for port & n_list
     if (argc != 3) {
         perror("Incorrect number of arguments");
         exit(-1);
@@ -17,21 +18,23 @@ int main(int argc, char *argv[]) {
     //stores the chars after the first numerical digits are taken.
     //e.g. "123abc" -> it will store "abc"
     char* endptr = NULL;
-    int sockfd = network_server_init(strtol(argv[1],endptr,10));
-    if (endptr){
+    int port = strtol(argv[1],endptr,10); //1024 <= port_range <= 98303 <- is this arbritrary? idk
+    if (endptr){ // catches bad port and return. 
         printf("Bad port number");
         return -1;
     }
+    int sockfd = network_server_init(port);
     //initiates table
-    endptr = NULL;
-    struct table_t* table = table_skel_init(strtol(argv[2],endptr,10));
-    if (endptr){
-        printf("Bad port number"); // 1024 <= port <= 98303
-        return -1;
-    }
+    struct table_t* table = table_skel_init(strtol(argv[2],NULL,10)); //malformed arg still works?? 
 
-    //handles bind, listening, and finally accept
+
+    //handles bind, listening, and finally accept -> reading loop
     struct sockaddr_in server_addr, client_addr;
+    //fill in the struct
+    server_addr.sin_family = AF_INET;                   // always set to AF_INET for IP interface
+    server_addr.sin_port = htons(atoi(argv[1]));        // the port in network byte order
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);    // binds to all local interfaces
+
     if (bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0){
         perror("Error in Bind()");
         close(sockfd);
@@ -44,10 +47,20 @@ int main(int argc, char *argv[]) {
         return -1;
     };
     printf("Server ready, waiting for connections");
-    
     if(accept(sockfd,(struct sockaddr *) &client_addr,sizeof(client_addr))){
         return network_main_loop(sockfd,table); // loop in here until it's out
     }
+
+
 }
 
-//TODO: handling SIGINT?
+// void signal_handler(int signal)
+// {
+//     if (signal == SIGPIPE){
+//         printf("Caught SIGPIPE, ignore and continue"); 
+//         // since we want it to ignore, eventually we can swap this function out for SIG_IGN instead...
+//         // however, for the sake of testing,there's a print here. 
+//         // SIGPIPE: Broken pipe: write to pipe with no readers
+//     }
+// }
+// //TODO: handling SIGINT?
