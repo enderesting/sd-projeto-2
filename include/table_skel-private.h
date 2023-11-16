84ab148c381b96e6ee4ff4dd7e5819b2fe159654
enderesting <datendza@gmail.com>
2023-11-16 15:22:21 +0000

    fixed a missing func definition

--- include/table_skel-private.h
+++ include/table_skel-private.h
@@ -8,9 +8,13 @@
 #ifndef _TABLE_SKEL_PRIVATE_H
 #define _TABLE_SKEL_PRIVATE_H
 
+#include <sys/time.h>
+#include <time.h>
 #include "sdmessage.pb-c.h"
+
 MessageT* respond_bad_op(MessageT* msg);
 MessageT* respond_err_in_exec(MessageT* msg);
 
+int return_time(struct timeval* tv);
 
 #endif
--- source/table_skel.c
+++ source/table_skel.c
@@ -8,8 +8,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include "table_skel.h"
-#include <sys/time.h>
-#include <time.h>
 
 struct timeval tv;
 /* Inicia o skeleton da tabela.
