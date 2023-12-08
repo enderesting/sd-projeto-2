bc3290417509ea6cb4147c16216e987c34126f3d
enderesting <datendza@gmail.com>
2023-12-08 17:39:32 +0000

    added structs helpful for serverside work

--- include/table_server-private.h
+++ include/table_server-private.h
@@ -10,19 +10,34 @@
 #include <signal.h>
 #include <stats.h>
 #include <pthread.h>
+#include <zookeeper/zookeeper.h>
 #include "mutex.h"
 
 extern volatile sig_atomic_t terminated;
 extern volatile sig_atomic_t connected;
+
+typedef struct server_address{
+    char* ip_port;
+    char* ip;
+    int port;
+} server_address;
 
 typedef struct server_resources {
     struct table_t* table;
     struct statistics_t* global_stats; // TODO Add pointer to stats struct
     mutex_locks* table_locks;
     mutex_locks* stats_locks;
+    // extras, for zk
+    zhandle_t* zh;
+    server_address* my_addr;
+    server_address* next_addr;
 } server_resources;
 
 extern server_resources resources;
+
+server_address* interpret_addr(char* addr_str);
+
+int boot_server(int n_lists);
 
 int init_server_resources(int n_lists);
 
