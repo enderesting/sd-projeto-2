6f07ff7c8bdde2d7f2ab3e5579cc5fc6125b0baa
aZul <fmdcosta.piano@gmail.com>
2023-12-12 19:53:35 +0000

    style: Fix spacing and column limit in client_stub

--- source/client_stub.c
+++ source/client_stub.c
@@ -60,27 +60,30 @@ int rtable_disconnect(struct rtable_t *rtable,int* connected) {
  * Retorna 0 (OK, em adição/substituição), ou -1 (erro).
  */
 int rtable_put(struct rtable_t *rtable, struct entry_t *entry,int* connected) {
-
     if (!rtable) return -1;
+
     MessageT* msg = (MessageT*) calloc(1, sizeof(MessageT));
     message_t__init(msg);
+
     EntryT* ent = (EntryT*) calloc(1, sizeof(EntryT));
     entry_t__init(ent);
+
     ent->key = strdup(entry->key);
     ent->value.len = entry->value->datasize;
     ent->value.data = malloc(ent->value.len);
     memcpy(ent->value.data, entry->value->data, entry->value->datasize);
+
     msg->opcode = MESSAGE_T__OPCODE__OP_PUT;
     msg->c_type = MESSAGE_T__C_TYPE__CT_ENTRY;
     msg->entry = ent;
 
     MessageT* res = network_send_receive(rtable, msg, connected);
-
     if (!res) return -1;
 
     if (res->opcode == MESSAGE_T__OPCODE__OP_BAD) {
         message_t__free_unpacked(res, NULL);
-        printf("Your function call was given incorrect and/or missing parameters\n");
+        printf("Your function call was given incorrect and/or missing"
+               "parameters\n");
         return -1;
     }
 
@@ -90,9 +93,9 @@ int rtable_put(struct rtable_t *rtable, struct entry_t *entry,int* connected) {
         return -1;
     }
 
-
     message_t__free_unpacked(msg, NULL);
     message_t__free_unpacked(res, NULL);
+
     return 0;
 }
 
@@ -105,18 +108,19 @@ struct data_t *rtable_get(struct rtable_t *rtable, char *key,int* connected) {
 
     MessageT* msg = (MessageT*) calloc(1, sizeof(MessageT));
     message_t__init(msg);
+
     msg->opcode = MESSAGE_T__OPCODE__OP_GET;
     msg->c_type = MESSAGE_T__C_TYPE__CT_KEY;
     msg->key = strdup(key);
 
     MessageT* res = network_send_receive(rtable, msg, connected);
     message_t__free_unpacked(msg, NULL);
-
     if (!res) return NULL;
   
     if (res->opcode == MESSAGE_T__OPCODE__OP_BAD) {
         message_t__free_unpacked(res, NULL);
-        printf("Your function call was given incorrect and/or missing parameters\n");
+        printf("Your function call was given incorrect and/or missing "
+               "parameters\n");
         return NULL;
     }
 
@@ -146,21 +150,21 @@ struct data_t *rtable_get(struct rtable_t *rtable, char *key,int* connected) {
  * Retorna 0 (OK), ou -1 (chave não encontrada ou erro).
  */
 int rtable_del(struct rtable_t *rtable, char *key,int* connected) {
-
     MessageT* msg = (MessageT*) calloc(1, sizeof(MessageT));
     message_t__init(msg);
+
     msg->opcode = MESSAGE_T__OPCODE__OP_DEL;
     msg->c_type = MESSAGE_T__C_TYPE__CT_KEY;
     msg->key = strdup(key);
 
     MessageT* res = network_send_receive(rtable, msg, connected);
     message_t__free_unpacked(msg, NULL);
-
     if (!res) return -1;
 
     if (res->opcode == MESSAGE_T__OPCODE__OP_BAD) {
         message_t__free_unpacked(res, NULL);
-        printf("Your function call was given incorrect and/or missing parameters\n");
+        printf("Your function call was given incorrect and/or missing "
+               "parameters\n");
         return -1;
     }
 
@@ -169,11 +173,11 @@ int rtable_del(struct rtable_t *rtable, char *key,int* connected) {
         message_t__free_unpacked(res, NULL);
         return -1;
     }
+
     if (res->c_type == MESSAGE_T__C_TYPE__CT_BAD) {
         message_t__free_unpacked(res, NULL);
         return -1;
-    }
-    else {
+    } else {
         message_t__free_unpacked(res, NULL);
         return 0;
     }
@@ -182,16 +186,15 @@ int rtable_del(struct rtable_t *rtable, char *key,int* connected) {
 /* Retorna o número de elementos contidos na tabela ou -1 em caso de erro.
  */
 int rtable_size(struct rtable_t *rtable,int* connected) {
-
     if(!rtable) return -1;
 
     MessageT* msg = (MessageT*) calloc(1, sizeof(MessageT));
     message_t__init(msg);
+
     msg->opcode = MESSAGE_T__OPCODE__OP_SIZE;
     msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
 
     MessageT* res = network_send_receive(rtable, msg, connected);
-
     if (!res) return -1;
 
     if (res->opcode == MESSAGE_T__OPCODE__OP_ERROR &&
@@ -200,7 +203,7 @@ int rtable_size(struct rtable_t *rtable,int* connected) {
         return -1;
     }
 
-    int size = (int)res->result;
+    int size = (int) res->result;
     message_t__free_unpacked(msg, NULL);
     message_t__free_unpacked(res, NULL);
 
@@ -212,11 +215,11 @@ int rtable_size(struct rtable_t *rtable,int* connected) {
  * Retorna NULL em caso de erro.
  */
 char **rtable_get_keys(struct rtable_t *rtable,int* connected) {
-
     if(!rtable) return NULL;
 
     MessageT* msg = (MessageT*) calloc(1, sizeof(MessageT));
     message_t__init(msg);
+
     msg->opcode = MESSAGE_T__OPCODE__OP_GETKEYS;
     msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
 
@@ -234,17 +237,18 @@ char **rtable_get_keys(struct rtable_t *rtable,int* connected) {
     for (int i = 0; i < res->n_keys; i++) {
         key_arr[i] = strdup(res->keys[i]);
     }
+
     key_arr[res->n_keys] = NULL;
 
     message_t__free_unpacked(msg, NULL);
     message_t__free_unpacked(res, NULL);
+
     return key_arr;
 }
 
 /* Liberta a memória alocada por rtable_get_keys().
  */
 void rtable_free_keys(char **keys) {
-
     int i = 0;
     char *key_ptr = keys[i];
     while (key_ptr != NULL) {
@@ -260,16 +264,15 @@ void rtable_free_keys(char **keys) {
  * um último elemento do array a NULL. Retorna NULL em caso de erro.
  */
 struct entry_t **rtable_get_table(struct rtable_t *rtable,int* connected) {
-
     if(!rtable) return NULL;
 
     MessageT* msg = (MessageT*) calloc(1, sizeof(MessageT));
     message_t__init(msg);
+
     msg->opcode = MESSAGE_T__OPCODE__OP_GETTABLE;
     msg->c_type = MESSAGE_T__C_TYPE__CT_NONE;
 
     MessageT* res = network_send_receive(rtable, msg, connected);
-
     if (!res) return NULL;
 
     if (res->opcode == MESSAGE_T__OPCODE__OP_ERROR &&
@@ -278,12 +281,17 @@ struct entry_t **rtable_get_table(struct rtable_t *rtable,int* connected) {
         return NULL;
     }
 
-    struct entry_t** entry_arr = (struct entry_t**) calloc(res->n_entries + 1, sizeof(struct entry_t*));
+    struct entry_t** entry_arr =
+        (struct entry_t**) calloc(res->n_entries + 1, sizeof(struct entry_t*));
     for (int i = 0; i < res->n_entries; i++) {
         entry_arr[i] = (struct entry_t*) malloc(sizeof(struct entry_t));
-        entry_arr[i]->value = (struct data_t*) malloc(sizeof(struct data_t));
+
         entry_arr[i]->key = strdup(res->entries[i]->key);
+
+        entry_arr[i]->value = (struct data_t*) malloc(sizeof(struct data_t));
+
         entry_arr[i]->value->datasize = res->entries[i]->value.len;
+
         entry_arr[i]->value->data = malloc(res->entries[i]->value.len);
         memcpy((entry_arr[i]->value->data), res->entries[i]->value.data, res->entries[i]->value.len);
     }
@@ -291,13 +299,13 @@ struct entry_t **rtable_get_table(struct rtable_t *rtable,int* connected) {
 
     message_t__free_unpacked(msg, NULL);
     message_t__free_unpacked(res, NULL);
+
     return entry_arr;
 }
 
 /* Liberta a memória alocada por rtable_get_table().
  */
 void rtable_free_entries(struct entry_t **entries) {
-
     int i = 0;
     struct entry_t *entry_ptr = entries[i];
     while (entry_ptr != NULL) {
@@ -305,6 +313,7 @@ void rtable_free_entries(struct entry_t **entries) {
         free(entry_ptr->value);
         free(entry_ptr->key);
         free(entry_ptr);
+
         i++;
         entry_ptr = entries[i];
     }
@@ -315,7 +324,6 @@ void rtable_free_entries(struct entry_t **entries) {
 /* Obtém as estatísticas do servidor. 
  */
 struct statistics_t *rtable_stats(struct rtable_t *rtable,int* connected) {
-
     if(!rtable) return NULL;
 
     MessageT* msg = (MessageT*) calloc(1, sizeof(MessageT));
@@ -333,12 +341,14 @@ struct statistics_t *rtable_stats(struct rtable_t *rtable,int* connected) {
         return NULL;
     }
 
-    struct statistics_t* stat = (struct statistics_t*) calloc(1, sizeof(struct statistics_t));
+    struct statistics_t* stat =
+        (struct statistics_t*) calloc(1, sizeof(struct statistics_t));
     stat->n_clientes = res->stats->n_clientes;
     stat->n_operacoes = res->stats->n_operacoes;
     stat->total_time = res->stats->total_time;
 
     message_t__free_unpacked(msg, NULL);
     message_t__free_unpacked(res, NULL);
+
     return stat;
 }
